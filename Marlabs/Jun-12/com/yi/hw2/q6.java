98c433287c33194667eeb9d5f5af2a9bcff6bf5d
jet <tyi100@syr.edu>
2020-06-13 16:33:37 -0400

    Final Version.

--- Marlabs/Jun-12/com/yi/hw2/q6.java
+++ Marlabs/Jun-12/com/yi/hw2/q6.java
@@ -9,13 +9,18 @@ public class q6 {
 			System.out.println("no duplicate");
 		}
 		Set<Character> s = new HashSet<Character>();
+		Set<Character> dup = new HashSet<>();
 		System.out.println("duplicate: ");
 		for (int i = 0; i < str.length(); i++) {
 			if (s.contains(str.charAt(i))) {
-				System.out.print(str.charAt(i) + " ");
+				//System.out.print(str.charAt(i) + " ");
+				dup.add(str.charAt(i));
 				continue;
 			}
 			s.add(str.charAt(i));
+		}
+		for (char c : dup) {
+			System.out.print(c + " ");
 		}
 		System.out.println();
 	}
